b5042b7b5e0e7494259e3a6c75e34bca666b2c4e
baixingqiang <bxq2011hust@qq.com>
2018-01-29 09:45:22 +0800

    update

--- codes/scritps/monitor-auto-ssh.sh
+++ codes/scritps/monitor-auto-ssh.sh
@@ -5,14 +5,19 @@
 # */1 * * * * bash /home/bxq/auto-ssh/auto-ssh.sh
 # * * * * * sleep 30; bash /home/bxq/autossh/monitor-auto-ssh.sh
 
-PROCESS_NAME='autossh'
+CHECK_STRING='$HOME/.ssh/auto'
 PROCESS_PATH=$PWD
-RemoteServer="192.168.1.1"
-START_PROCESS="autossh -M 55667 -NR *:55666:localhost:22 autossh@${RemoteServer} -i ${HOME}/.ssh/autossh_rsa"
+REMOTE_SERVER="123.207.14.190"
+PORT=55664
+REMOTE_USER="autossh"
+REMOTE_USER_PRIVATE_KEY="$HOME/.ssh/autossh_rsa"
+START_PROCESS="autossh -M 55665 -NR *:${PORT}:localhost:22 ${REMOTE_USER}@${REMOTE_SERVER} -i ${REMOTE_USER_PRIVATE_KEY}"
+
+echo "command : ${START_PROCESS}"
 
 proc_num()                      #查询进程数量
 {
-    num=`ps -ef | grep ${PROCESS_NAME} | grep -v grep | wc -l`
+    num=`ps -ef | grep ${CHECK_STRING} | grep -v grep | wc -l`
     return $num
 }
 
@@ -20,8 +25,10 @@ proc_num
 number=$?
 if [ $number -eq 0 ]            #如果进程数量为0
 then                            #重新启动服务
-    echo "Restarting ${PROCESS_NAME} ..."
+    echo "Restarting ${CHECK_STRING} ..."
     echo $(date '+%Y-%m-%d %T') >> ${PROCESS_PATH}/restart.log
     (${START_PROCESS}) &
     echo "over"
+else
+    echo "autossh is already running."
 fi
